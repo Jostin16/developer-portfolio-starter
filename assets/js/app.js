c71c691e977a74c4776a5853e98f7528d0a768bc
Jostin16 <jostinmartinez1605@gmail.com>
2021-11-01 01:31:51 -0500

    scroll-done

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,26 @@ scrollUp.addEventListener("click", () =>{
         behavior: "smooth"
     });
 });
+
+
+//hamburger section
+
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul"); /*the nav links altogether*/
+const nav = document.querySelector("nav"); /*whole nav element */
+
+burger.addEventListener("click", ()=>{
+    ul.classList.toggle("show");
+});
+
+//close the burger when a link is clicked
+
+const navLink = document.querySelectorAll(".nav-link"); /* select all navs links li */
+
+navLink.forEach((link) =>{
+    link.addEventListener("click",()=>{
+        ul.classList.remove("show");
+    });
+});
+
+/*How to Make the Hero Section Responsive*/
