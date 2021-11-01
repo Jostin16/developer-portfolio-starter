2c7f4fdf5dbe921825c731f3f06a44491248ea05
Jostin16 <jostinmartinez1605@gmail.com>
2021-11-01 00:04:16 -0500

    scrollUp

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+//functionality -> scroll
+
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () =>{
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+});
