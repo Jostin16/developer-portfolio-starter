3417ecfd642f1fa63df5e8975b4033282ac79c81
Jostin16 <jostinmartinez1605@gmail.com>
2021-11-02 00:18:30 -0500

    dark-mode

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,4 +31,29 @@ navLink.forEach((link) =>{
     });
 });
 
+/*Switch*/
+
+const btnSwitch = document.querySelector("#switch");
+
+btnSwitch.addEventListener('click',()=>{
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+
+    //save the dark mode in LocalStorage
+    if(document.body.classList.contains('dark')){
+        localStorage.setItem('dark-mode', 'true'); //saving the mode
+    }else{
+        localStorage.setItem('dark-mode', 'false');
+    }
+});
+
+//get the current mode & saving the side of the switch
+
+if(localStorage.getItem('dark-mode')==='true'){
+    document.body.classList.add('dark'); 
+    btnSwitch.classList.add('active');
+}else{
+    document.body.classList.remove('dark');
+    btnSwitch.classList.remove('active');
+}
 
